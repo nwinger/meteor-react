7ba0b41bd990bff4e7d0533dc772bcb10b8e5c88
Nicklas Winger <winger.nicklas@gmail.com>
2017-03-18 11:19:47 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(function () {
+    let name = 'Nicklas';
+    let jsx = <p>Hello {name}!</p>;
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
