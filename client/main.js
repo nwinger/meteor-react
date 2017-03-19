d454d46ec9e570ed3566c2ab9c0507736e5a7984
Nicklas Winger <winger.nicklas@gmail.com>
2017-03-19 22:44:40 +0100

    Implement players collection and basic connection with mongodb and minimongo client side

--- client/main.js
+++ client/main.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
 
-Meteor.startup(function () {
-    let name = 'Nicklas';
-    let jsx = <p>Hello {name}!</p>;
-    ReactDOM.render(jsx, document.getElementById('app'));
+import { Players } from "./../imports/api/players";
+
+const renderPlayers = function(playersList) {
+  return playersList.map(function(player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+  });
+};
+
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    let players = Players.find().fetch();
+    let title = "Score Keep";
+    let name = "Nicklas";
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById("app"));
+  });
+
+  Players.insert({ name: "Andrew", score: -1 });
 });
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
-// import './../imports/utils';
-import { greetUser } from './../imports/utils';
-import add from './../imports/math';
+import { Meteor } from "meteor/meteor";
+import { Players } from "./../imports/api/players";
 
-console.log('Log from /server/main.js');
-console.log(add(7, 7));
+Meteor.startup(function() {
+  //   Players.insert({ name: "Julie", score: 99 });
+  //   console.log(Players.find().fetch());
+});
