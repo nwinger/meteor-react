59f86890808ee5861166ce4f4105a611b7d09164
Nicklas Winger <winger.nicklas@gmail.com>
2017-03-22 22:18:57 +0100

    Add possibility to increment and decrement score

--- client/main.js
+++ client/main.js
@@ -10,7 +10,17 @@ const renderPlayers = playersList => {
     return (
       <p key={player._id}>
         {player.name} has {player.score} point(s).
-        <button onClick={() => Players.remove({ _id: player._id })}>
+        <button
+          onClick={() => Players.update(player._id, { $inc: { score: -1 } })}
+        >
+          -1
+        </button>
+        <button
+          onClick={() => Players.update(player._id, { $inc: { score: 1 } })}
+        >
+          +1
+        </button>
+        <button onClick={() => Players.remove(player._id)}>
           X
         </button>
       </p>
